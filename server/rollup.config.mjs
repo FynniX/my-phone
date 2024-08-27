7c8dc512127abda62270926f2c74948e9075398d
Fynn Haupt <info@fynnhaupt.de>
2024-08-27 22:22:19 +0200

    added external dep to rollup config

--- server/rollup.config.mjs
+++ server/rollup.config.mjs
@@ -1,8 +1,9 @@
 // rollup.config.js
-import typescript from '@rollup/plugin-typescript'
+import typescript from "@rollup/plugin-typescript"
 
 export default {
   input: 'src/index.ts',
+  externals: ['@prisma/client'],
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
