0dc19814185cec284db675de73465d1105680ba9
Fynn Haupt <info@fynnhaupt.de>
2024-08-27 11:06:07 +0200

    added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Fynn Haupt <info@fynnhaupt.de>'
+description 'A mobile phone in Apple iPhone 15 Style in FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+server_script 'server/dist/bundle.js'
+
+ui_page 'ui/dist/index.html'
+files {
+    'ui/dist/**/*'
+}
+
+
+
+
